ac704d28e86c7f041a48b4e2cf73601c5f142701
itz-Hiru <hirumithagithub@gmail.com>
2025-03-22 12:20:06 +0530

    Created two buttons

--- app/src/main/java/com/hiru/aurudunakathseettuwa/MainActivity.java
+++ app/src/main/java/com/hiru/aurudunakathseettuwa/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hiru.aurudunakathseettuwa;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.home);
+        setContentView(R.layout.welcome);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        RelativeLayout sinhalaBtn = findViewById(R.id.sinhala_btn);
+        RelativeLayout tamilBtn = findViewById(R.id.tamil_btn);
+
+        sinhalaBtn.setOnClickListener(v -> setContentView(R.layout.home_sinhala));
+        tamilBtn.setOnClickListener(v -> setContentView(R.layout.home_tamil));
     }
 }
