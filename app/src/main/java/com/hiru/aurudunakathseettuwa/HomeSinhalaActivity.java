b69b65d6e45324fbe7b058413807fc8f72d6b869
itz-Hiru <hirumithagithub@gmail.com>
2025-03-22 14:06:26 +0530

    Created sinhala nakath

--- app/src/main/java/com/hiru/aurudunakathseettuwa/HomeSinhalaActivity.java
+++ app/src/main/java/com/hiru/aurudunakathseettuwa/HomeSinhalaActivity.java
@@ -3,6 +3,10 @@ package com.hiru.aurudunakathseettuwa;
 import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
 
 public class HomeSinhalaActivity extends AppCompatActivity {
 
@@ -11,5 +15,14 @@ public class HomeSinhalaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.home_sinhala);
+
+        View sinhalaLayout = findViewById(R.id.sinhala_layout);
+        if (sinhalaLayout != null) {
+            ViewCompat.setOnApplyWindowInsetsListener(sinhalaLayout, (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+        }
     }
 }
--- app/src/main/java/com/hiru/aurudunakathseettuwa/HomeTamilActivity.java
+++ app/src/main/java/com/hiru/aurudunakathseettuwa/HomeTamilActivity.java
@@ -1,8 +1,13 @@
 package com.hiru.aurudunakathseettuwa;
 
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class HomeTamilActivity extends AppCompatActivity {
 
@@ -11,5 +16,14 @@ public class HomeTamilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.home_tamil);
+
+        View sinhalaLayout = findViewById(R.id.tamil_layout);
+        if (sinhalaLayout != null) {
+            ViewCompat.setOnApplyWindowInsetsListener(sinhalaLayout, (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+        }
     }
 }
